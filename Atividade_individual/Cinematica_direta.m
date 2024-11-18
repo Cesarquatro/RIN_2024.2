4dd197597cf2f818f1a2ab9a4c8c30f20511daaf
Cesar Augusto M. C. da Silva <caugustomendes653@gmail.com>
2024-11-17 23:50:46 -0300

    Testes iniciais

--- Atividade_individual/Cinematica_direta.m
+++ Atividade_individual/Cinematica_direta.m
@@ -1,10 +1,19 @@
 clc; clear; close all;
-%% 
-alpha = 2;
-a = 10;
-d = 20;
-offset = 0;
+%% Ⅰ) Deininido parâmetros
+l      = [166      , 135 ,250  , 160  , 72   , 106.5]; %[mm]
+alpha  = [0        , pi/2, 0   , 0    , -pi/2, 0];
+a      = [0        , 0   , l(3), l(4) , 0    , 0];
+d      = [0        , pi/2, 0   , -pi/2, 0    , 0];
+offset = [l(1)+l(2), 0   , 0   , 0    , l(3) , l(6)];
 
-L(1) = RevoluteMDH('alpha', alpha, 'a', a, 'd', d, 'offset', offset);
-myrobot = SerialLink(L,'name','myrobot')
+%% Ⅱ) Construção do Robo:
+L = cell(1, length(l)); % Use uma célula ao invés de um vetor numérico
+
+for i = 1:1:length(l)
+    L{i} = RevoluteMDH('alpha', alpha(i), 'a', a(i), 'd', d(i), 'offset', offset(i));
+end
+L = [L{:}];
+
+myrobot = SerialLink(L,'name','myrobot[5DOF]')
 myrobot.teach('rpy/zyx')
+q = L.getpos()
