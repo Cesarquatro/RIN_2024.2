00de83a75d915135bcbf267c909ae0ee0de13616
Cesar Augusto M. C. da Silva <caugustomendes653@gmail.com>
2024-11-20 23:28:23 -0300

    Ainda não está batendo os valores

--- Atividade_individual/Cinematica_inversa.m
+++ Atividade_individual/Cinematica_inversa.m
@@ -12,30 +12,48 @@ clc; clear; close all;
 % Comprimentos
 l      = [166, 135, 250, 160, 72, 105.64]; %[mm]
 
-% Entrada dos ângulos das juntas em graus pelo usuário:
-disp("Coloque os ângulos das juntas em graus:")
-th1 = deg2rad(input("θ₁: "));
-th2 = deg2rad(input("θ₂: "));
-th3 = deg2rad(input("θ₃: "));
-th4 = deg2rad(input("θ₄: "));
-th5 = deg2rad(input("θ₅: "));
+% Entrada das posições em mm pelo usuário:
+disp("Coloque as posições em mm do TCP:")
+PX = input("X: ");
+PY = input("Y: ");
+PZ = input("Z: ");
 
 % Resto dos parâmetros
 alphas = [0, -pi/2, 0, 0, pi/2, 0];       % α (i-1) [rad]
 as     = [0, 0, l(3), l(4), 0, 0];        % a (i-1) [mm]
-thetas = [th1, th2-(pi/2), th3, th4+(pi/2), th5, 0]; % θ (i) [rad]
+%thetas = [th1, th2-(pi/2), th3, th4+(pi/2), th5, 0]; % θ (i) [rad]
 ds     = [l(1)+l(2), 0, 0, 0, l(5), l(6)]; % d (i) [mm]
 offsets = [0, -pi/2, 0, +pi/2, 0, 0];
+
 for ii=1:1:6
     L(ii) = RevoluteMDH('alpha',alphas(ii), 'a', as(ii), 'd', ds(ii), 'offset', offsets(ii));
 end
 myrobot = SerialLink(L,'name','myrobot');
 myrobot.teach('rpy/zyx')
 
-Q0 = [0, 0, 0, 0, 0, 0];
-Qi = myrobot.ikunc(Ttot,Q0);
-Qi = Qi*180/pi;
-Js = [Qi(1), Qi(2), Qi(3), Qi(4), Qi(5)+Qi(6)];
+T = [ 1 0 0 PX;
+      0 1 0 PY;
+      0 0 1 PZ;
+      0 0 0 1]
 
-fprintf("\n\nCinemática Inversa do Robô: ");
-disp(Js)
+n = 0;
+
+% Ângulos iniciais de referência
+Q0 = [0, 0, 0, 0, 0, 0]; % Chute inicial
+
+% Calculando os ângulos das juntas
+try
+    Qi = myrobot.ikunc(T, Q0); % Utilizando a cinemática inversa universal
+catch
+    error("Não foi possível calcular a cinemática inversa para a posição fornecida.");
+end
+
+Qi_deg = rad2deg(Qi);
+
+%% Ⅲ) Resultado
+disp("Ângulos das juntas (em graus):");
+fprintf("θ₁: %.2f°\n", Qi_deg(1));
+fprintf("θ₂: %.2f°\n", Qi_deg(2));
+fprintf("θ₃: %.2f°\n", Qi_deg(3));
+fprintf("θ₄: %.2f°\n", Qi_deg(4));
+fprintf("θ₅: %.2f°\n", Qi_deg(5)+Qi_deg(6));
